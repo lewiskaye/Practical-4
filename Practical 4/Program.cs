1687e778d2fdfce24dc1af945b83ebbdc5e269a0
lewiskaye <lewiskaye@hotmail.co.uk>
2021-10-08 10:03:36 +0100

    first commit

---
+++ Practical 4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a test comment
+
+        }
+    }
+}
